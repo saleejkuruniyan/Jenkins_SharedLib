4cbd8ae395a86e5f774464ecb9bcdb23eb661d6c
Saleej Kuruniyan <saleejkuruniyan@gmail.com>
2025-03-27 19:34:05 +0300

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,6 +1,6 @@
 // Define function
 def call(String ProjectName, String ImageTag, String dockerHubCred){
-  withCredentials([usernamePassword(credentialsId: ${dockerHubCred}, passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+  withCredentials([usernamePassword(credentialsId: "${dockerHubCred}", passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
       sh "docker build -t ${dockerhubuser}/${ProjectName}:${ImageTag} ."
   }
 }
