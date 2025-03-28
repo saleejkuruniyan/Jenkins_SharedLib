7db21eacc948facfa57d1219d7ad2e62f2072511
Saleej Kuruniyan <saleejkuruniyan@gmail.com>
2025-03-28 13:20:00 +0300

    Update code_checkout_with_cred.groovy

--- vars/code_checkout_with_cred.groovy
+++ vars/code_checkout_with_cred.groovy
@@ -1,16 +1,3 @@
 def call(String GitUrl, String GitBranch, String GithubCredId){
-    withCredentials([gitUsernamePassword(credentialsId: "${GithubCredId}", gitToolName: 'Default')]) {
-        sh """
-            git config --global credential.helper 'cache'
-            
-            # Clone the repository using the configured credentials
-            git clone ${GitUrl}
-            
-            // # Extract repo name from Git URL and enter the directory
-            // cd \$(basename ${GitUrl} .git)
-            
-            # Checkout the required branch
-            git checkout ${GitBranch}
-        """
-    }
+  git url: "${GitUrl}", branch: "${GitBranch}", credentialsId: "${GithubCredId}"
 }
