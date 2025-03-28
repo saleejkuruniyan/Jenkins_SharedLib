51a33ecefe320a15834103c7817e7bea74e2faaf
Saleej Kuruniyan <saleejkuruniyan@gmail.com>
2025-03-28 02:25:27 +0000

    Test Fix

--- vars/code_checkout_with_cred.groovy
+++ vars/code_checkout_with_cred.groovy
@@ -1,8 +1,3 @@
 def call(String GitUrl, String GitBranch){
   git url: "${GitUrl}", branch: "${GitBranch}"
 }
-def call(String GitUrl, String GitBranch, String Github-cred){
-  withCredentials([gitUsernamePassword(credentialsId: "${Github-cred}", gitToolName: 'Default')]) {
-      git url: "${GitUrl}", branch: "${GitBranch}"
-  }
-}
