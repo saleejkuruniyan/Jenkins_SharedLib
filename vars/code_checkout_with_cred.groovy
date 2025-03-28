550c1ebafed097c9590323cf5a1e3c12d46328f6
Saleej Kuruniyan <saleejkuruniyan@gmail.com>
2025-03-28 02:29:39 +0000

    Test Fix

--- vars/code_checkout_with_cred.groovy
+++ vars/code_checkout_with_cred.groovy
@@ -1,3 +1,9 @@
-def call(String GitUrl, String GitBranch){
-  git url: "${GitUrl}", branch: "${GitBranch}"
+def call(String GitUrl, String GitBranch, String GithubCredId){
+    withCredentials([usernamePassword(credentialsId: "${GithubCredId}", usernameVariable: 'GIT_USER', passwordVariable: 'GIT_TOKEN')]) {
+        sh """
+            git config --global credential.helper 'store'
+            git clone https://\${GIT_USER}:\${GIT_TOKEN}@${GitUrl}.git
+            git checkout ${GitBranch}
+        """
+    }
 }
