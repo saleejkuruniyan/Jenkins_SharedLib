b2d473740fa768c4e51c10d988c050ed71ee1b5e
Saleej Kuruniyan <saleejkuruniyan@gmail.com>
2025-05-04 13:11:28 +0300

    Update owasp_dependency_api.groovy

--- vars/owasp_dependency_api.groovy
+++ vars/owasp_dependency_api.groovy
@@ -1,6 +1,9 @@
 def call(String apiKey, String cacheDir) {
     withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
         sh """
+
+        export PATH=/usr/share/dependency-check/bin:$PATH
+        
         dependency-check.sh \\
             --project "${env.JOB_NAME}" \\
             --scan "${env.WORKSPACE}" \\
