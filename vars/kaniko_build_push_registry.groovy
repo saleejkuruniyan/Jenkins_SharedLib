90e329da92e7e28e4ffc7523f543a0446cc1bbf9
Saleej Kuruniyan <saleejkuruniyan@gmail.com>
2025-05-06 07:23:01 +0000

    bugFix

--- vars/kaniko_build_push_registry.groovy
+++ vars/kaniko_build_push_registry.groovy
@@ -1,26 +1,22 @@
-def call(String RegistryURL, String ProjectName, String registryCred, List<List<String>> builds) {
+def call(String registryPath, List<List<String>> builds) {
     def scriptLines = []
 
-    withCredentials([usernamePassword(credentialsId: "${registryCred}", passwordVariable: 'registrypass', usernameVariable: 'registryuser')]) {
-        for (def args : builds) {
-            String DockerfilePath = args[0]
-            String RepoName = args[1]
-            String ImageTag = args[2]
-            def destination = "${RegistryURL}/${ProjectName}/${RepoName}:${ImageTag}"
+    for (def args : builds) {
+        String DockerfilePath = args[0]
+        String RepoName = args[1]
+        String ImageTag = args[2]
+        def destination = "${RegistryPath}/${RepoName}:${ImageTag}"
 
-            scriptLines << """
-            echo "Building and pushing image for ${RepoName}:${ImageTag}..."
-            /kaniko/executor \\
-                --dockerfile=${DockerfilePath}/Dockerfile \\
-                --context=${WORKSPACE}/${DockerfilePath} \\
-                --destination=${destination} \\
-                --skip-tls-verify \\
-                --build-arg DOCKER_USERNAME=${registryuser} \\
-                --build-arg DOCKER_PASSWORD=${registrypass}
-            rm -rf /kaniko/0/*
-            """
-        }
-
-        sh scriptLines.join("\n")
+        scriptLines << """
+        echo "Building image for ${RepoName}:${ImageTag}..."
+        /kaniko/executor \\
+            --dockerfile=${DockerfilePath}/Dockerfile \\
+            --context=${WORKSPACE}/${DockerfilePath} \\
+            --destination=${destination} \\
+            --skip-tls-verify
+        rm -rf /kaniko/0/*
+        """
     }
+
+    sh scriptLines.join("\n")
 }
