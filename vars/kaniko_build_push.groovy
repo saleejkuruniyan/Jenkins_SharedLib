ef3628439af9bf6abc153a61a9ea7dc4c78f9ac2
Saleej Kuruniyan <saleejkuruniyan@gmail.com>
2025-05-06 05:15:29 +0000

    Minor bug fix

--- vars/kaniko_build_push.groovy
+++ vars/kaniko_build_push.groovy
@@ -1,10 +1,22 @@
-def call(String RegistryURL, String ProjectName, String DockerfilePath, String RepoName, String ImageTag){
-    def destination = "${RegistryURL}/${ProjectName}/${RepoName}:${ImageTag}"
-        sh """
-            /kaniko/executor \\
-                --dockerfile=${DockerfilePath}/Dockerfile \\
-                --context=${WORKSPACE}/${DockerfilePath} \\
-                --destination=${destination} \\
-                --skip-tls-verify --cleanup
+def kaniko_build_push(String RegistryURL, String ProjectName, List<List<String>> builds) {
+    def scriptLines = []
+
+    for (def args : builds) {
+        String DockerfilePath = args[0]
+        String RepoName = args[1]
+        String ImageTag = args[2]
+        def destination = "${RegistryURL}/${ProjectName}/${RepoName}:${ImageTag}"
+
+        scriptLines << """
+        echo "Building image for ${RepoName}:${ImageTag}..."
+        /kaniko/executor \\
+            --dockerfile=${DockerfilePath}/Dockerfile \\
+            --context=${WORKSPACE}/${DockerfilePath} \\
+            --destination=${destination} \\
+            --skip-tls-verify
+        rm -rf /kaniko/0/*
         """
+    }
+
+    sh scriptLines.join("\n")
 }
