2cf8bc347235847862ce7bf7c45601335fb3b24a
Saleej Kuruniyan <saleejkuruniyan@gmail.com>
2025-07-13 10:20:48 +0300

    Update kaniko_build_push_registry.groovy

--- vars/kaniko_build_push_registry.groovy
+++ vars/kaniko_build_push_registry.groovy
@@ -1,22 +1,19 @@
 def call(String RegistryPath, List<List<String>> builds) {
-    def scriptLines = []
-
     for (def args : builds) {
-        String DockerfilePath = args[0]
-        String RepoName = args[1]
-        String ImageTag = args[2]
+        String DockerfilePath = args[0]   // e.g., "backend"
+        String RepoName = args[1]         // e.g., "wanderlust-backend-beta"
+        String ImageTag = args[2]         // e.g., "1.0"
+
         def destination = "${RegistryPath}/${RepoName}:${ImageTag}"
 
-        scriptLines << """
-        echo "Building image for ${RepoName}:${ImageTag}..."
-        /kaniko/executor \\
-            --dockerfile=${DockerfilePath}/Dockerfile \\
-            --context=\$(pwd)/${DockerfilePath} \\
-            --destination=${destination} \\
-            --skip-tls-verify
-        rm -rf /kaniko/0/*
+        sh """
+            echo "Building image for ${RepoName}:${ImageTag}..."
+            /kaniko/executor \\
+                --dockerfile=${DockerfilePath}/Dockerfile \\
+                --context=\$(pwd)/${DockerfilePath} \\
+                --destination=${destination} \\
+                --skip-tls-verify
+            rm -rf /kaniko/0/*
         """
     }
-
-    sh scriptLines.join("\n")
 }
